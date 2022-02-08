e9d55f8c72dda7d96e22e546cf4fd07962f6d51f
JuanK003 <juankdev003@gmail.com>
2022-02-08 15:39:26 -0600

    Centrado de forms

--- administradores_memoria_JuanCarlosPalacios_JoelGramajo/Form1.Designer.cs
+++ administradores_memoria_JuanCarlosPalacios_JoelGramajo/Form1.Designer.cs
@@ -42,9 +42,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(195, 402);
+            this.button1.Location = new System.Drawing.Point(130, 261);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(200, 118);
+            this.button1.Size = new System.Drawing.Size(133, 77);
             this.button1.TabIndex = 0;
             this.button1.Text = "ESQUEMA DE USUARIO ÚNICO";
             this.button1.UseVisualStyleBackColor = true;
@@ -52,9 +53,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(426, 402);
+            this.button2.Location = new System.Drawing.Point(284, 261);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(200, 118);
+            this.button2.Size = new System.Drawing.Size(133, 77);
             this.button2.TabIndex = 1;
             this.button2.Text = "PARTICIONES FIJAS";
             this.button2.UseVisualStyleBackColor = true;
@@ -62,9 +64,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(195, 572);
+            this.button3.Location = new System.Drawing.Point(130, 372);
+            this.button3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(200, 118);
+            this.button3.Size = new System.Drawing.Size(133, 77);
             this.button3.TabIndex = 2;
             this.button3.Text = "ASIGNACIÓN DEL PRIMER AJUSTE";
             this.button3.UseVisualStyleBackColor = true;
@@ -72,9 +75,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(426, 572);
+            this.button4.Location = new System.Drawing.Point(284, 372);
+            this.button4.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(200, 118);
+            this.button4.Size = new System.Drawing.Size(133, 77);
             this.button4.TabIndex = 3;
             this.button4.Text = "ASIGNACIÓN DEL MEJOR AJUSTE";
             this.button4.UseVisualStyleBackColor = true;
@@ -83,9 +87,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Copperplate Gothic Bold", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(16, 298);
+            this.label1.Location = new System.Drawing.Point(11, 194);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(784, 35);
+            this.label1.Size = new System.Drawing.Size(523, 23);
             this.label1.TabIndex = 5;
             this.label1.Text = "Administradores de Memoria";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -93,9 +98,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Copperplate Gothic Light", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(22, 334);
+            this.label2.Location = new System.Drawing.Point(15, 217);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(778, 37);
+            this.label2.Size = new System.Drawing.Size(519, 24);
             this.label2.TabIndex = 6;
             this.label2.Text = "primeras generaciones";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -103,30 +109,30 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(555, 747);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Location = new System.Drawing.Point(370, 486);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(263, 20);
+            this.label3.Size = new System.Drawing.Size(175, 13);
             this.label3.TabIndex = 7;
             this.label3.Text = "Joel Gramajo - Juan Carlos Palacios";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = global::administradores_memoria_JuanCarlosPalacios_JoelGramajo.Properties.Resources.logo_meso;
-            this.pictureBox1.Location = new System.Drawing.Point(284, 35);
+            this.pictureBox1.Location = new System.Drawing.Point(189, 23);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(254, 232);
+            this.pictureBox1.Size = new System.Drawing.Size(169, 151);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(831, 787);
+            this.ClientSize = new System.Drawing.Size(554, 512);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -135,8 +141,10 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- administradores_memoria_JuanCarlosPalacios_JoelGramajo/Form1.cs
+++ administradores_memoria_JuanCarlosPalacios_JoelGramajo/Form1.cs
@@ -20,25 +20,34 @@ namespace administradores_memoria_JuanCarlosPalacios_JoelGramajo
         private void button1_Click(object sender, EventArgs e)
         {
             esquema_usuario_unico feuu = new esquema_usuario_unico();
+            feuu.StartPosition = FormStartPosition.CenterParent;
             feuu.ShowDialog();
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             particiones_fijas fpf = new particiones_fijas();
+            fpf.StartPosition = FormStartPosition.CenterParent;
             fpf.ShowDialog();
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
             PrimerAjuste fpa = new PrimerAjuste();
+            fpa.StartPosition = FormStartPosition.CenterParent;
             fpa.ShowDialog();
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
             MejorAjuste fma = new MejorAjuste();
+            fma.StartPosition = FormStartPosition.CenterParent;
             fma.ShowDialog();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
